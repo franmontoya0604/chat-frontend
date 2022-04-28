07e08edb9c765d9187d48ea4d5079874d3025a7a
franmontoya <franciscomontoya000@gmail.com>
2022-04-28 20:15:22 -0300

    spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className="spinner-border text-primary" role="status">
+    <div className="spinner-border text-primary spinner" role="status">
   <span className="visually-hidden">Loading...</span>
 </div>
   )
